75a5390c6b65c9a23214888ca318b40c37d6f7d1
jiminy-kirket <61626164+jiminy-kirket@users.noreply.github.com>
2021-05-21 11:57:34 -0400

    initial commit

---
+++ ShoppingApp/SignInViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SignInViewController.swift
+//  ShoppingApp
+//
+//  Created by Robert Olieman on 5/21/21.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    init() {
+        super.init(nibName: "SignInViewController", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
