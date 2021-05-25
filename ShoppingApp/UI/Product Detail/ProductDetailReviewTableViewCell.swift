206609babbeeb08bfa7fc57adad1598ea203e6ad
jiminy-kirket <61626164+jiminy-kirket@users.noreply.github.com>
2021-05-25 14:50:32 -0400

    working on product detail

---
+++ ShoppingApp/UI/Product Detail/ProductDetailReviewTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ProductDetailReviewTableViewCell.swift
+//  ShoppingApp
+//
+//  Created by Robert Olieman on 5/25/21.
+//
+
+import UIKit
+
+class ProductDetailReviewTableViewCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
