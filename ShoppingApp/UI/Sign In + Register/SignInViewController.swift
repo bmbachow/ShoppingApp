b3ad1a9880e373ba09ea95fcdc79d98fde3f9ef5
dprater1 <dprater@ucmerced.edu>
2021-05-25 09:11:44 -0700

    added Sign Up Functionality

--- ShoppingApp/UI/Sign In + Register/SignInViewController.swift
+++ ShoppingApp/UI/Sign In + Register/SignInViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class SignInViewController: BaseViewController {
+class SignInViewController: UserTabViewController {
 
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var pass: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,6 +25,27 @@ class SignInViewController: BaseViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    @IBAction func signIn(_ sender: Any) {
+        guard let userName = self.userName.textnoEmptyString,
+              let pass = self.pass.textnoEmptyString else{
+            return
+        }
+        remoteAPI.authenticateAndGetUser(emailOrPhoneNumber: userName, password: pass, success: { useroptional in
+            guard let user = useroptional else {
+                return presentBasicAlert(title: "Invalid Sign-In", message: "Check the email/phone or password", onDismiss: nil)
+                
+            }
+            self.user = user
+            self.presentingViewController?.dismiss(animated: true, completion: nil)
+            
+        }, failure: {error in
+            
+            print(error.localizedDescription)
+            
+        })
+        
+        
+    }
     
     /*
     // MARK: - Navigation
