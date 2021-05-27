a80b0328dcbe4665fd1ec1774c95a08d499ac92e
jiminy-kirket <61626164+jiminy-kirket@users.noreply.github.com>
2021-05-27 10:57:49 -0400

    added swiping and stuff to ad image

--- ShoppingApp/UI/Shopping/ShoppingAdTableViewCell.swift
+++ ShoppingApp/UI/Shopping/ShoppingAdTableViewCell.swift
@@ -16,32 +16,71 @@ class ShoppingAdTableViewCell: UITableViewCell {
     
     var images : [UIImage] = [UIImage(named: "shopping0")!, UIImage(named: "shopping1")!, UIImage(named: "shopping2")!]
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
+    var currentImageIndex: Int {
+        self.images.firstIndex(of: self.shoppingImage.image!)!
     }
     
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    func nextImage(forward: Bool) -> UIImage {
+        var index: Int
+        let currentIndex = self.currentImageIndex
+        if forward {
+            if currentIndex < self.images.count - 1 {
+                index = currentIndex + 1
+            } else {
+                index = 0
+            }
+        } else {
+            if currentIndex > 0 {
+                index = currentIndex - 1
+            } else {
+                index = self.images.count - 1
+            }
+        }
+        return self.images[index]
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.shoppingImage.image = images[0]
+        self.pageCon.numberOfPages = self.images.count
         
-        // Configure the view for the selected state
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(self.swipedImageLeft))
+        leftSwipe.direction = .left
+        
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(self.swipedImageRight))
+        rightSwipe.direction = .right
+        
+        self.shoppingImage.gestureRecognizers = [leftSwipe, rightSwipe]
+        self.shoppingImage.isUserInteractionEnabled = true
+    }
+    
+    @objc func swipedImageLeft(_ sender: UISwipeGestureRecognizer) {
+        self.changeToImage(forward: true, options: .transitionCrossDissolve)
+        self.startAdTimer()
+    }
+    
+    @objc func swipedImageRight(_ sender: UISwipeGestureRecognizer) {
+        self.changeToImage(forward: false, options: .transitionCrossDissolve)
+        self.startAdTimer()
     }
     
     func startAdTimer(){
         self.timer.invalidate()
-        self.timer = Timer.scheduledTimer(withTimeInterval: 12.0, repeats: true) { timer in
-            UIView.transition(with: self.shoppingImage,
-                              duration: 0.5,
-                              options: .transitionCrossDissolve,
-                              animations: {
-                                self.shoppingImage.image = self.images.randomElement()
-                                self.pageCon.currentPage += 1
-                                if(self.pageCon.currentPage > 3){
-                                    self.pageCon.currentPage = 0
-                                }
-                              },
-                              completion: nil)
+        self.timer = Timer.scheduledTimer(withTimeInterval: 6.0, repeats: true) { timer in
+            self.changeToImage(forward: true, options: .transitionCrossDissolve)
         }
+    }
+    
+    func changeToImage(forward: Bool, options: UIView.AnimationOptions) {
+        UIView.transition(with: self.shoppingImage,
+                          duration: 0.5,
+                          options: options,
+                          animations: {
+                            self.shoppingImage.image = self.nextImage(forward: true)
+                          },
+                          completion: nil)
+        
+        self.pageCon.currentPage = self.currentImageIndex
     }
     
     func invalidateTimer(){
