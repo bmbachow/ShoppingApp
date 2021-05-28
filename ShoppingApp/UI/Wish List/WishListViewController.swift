6806599f70902510d17663f040b2959ea18a8d9e
Brian Bachow <bmbachow@gmail.com>
2021-05-28 08:12:14 -0700

    wish list work

--- ShoppingApp/UI/Wish List/WishListViewController.swift
+++ ShoppingApp/UI/Wish List/WishListViewController.swift
@@ -51,7 +51,24 @@ class WishListViewController: UserTabViewController, UITableViewDelegate, UITabl
     }
     
     func tappedDeleteButton(inCell cell: WishListTableViewCell) {
- 
+        guard let indexPath = wishListTable.indexPath(for: cell) else {
+            return
+        }
+        
+        wishListTable.beginUpdates()
+        
+        remoteAPI.deleteProductFromWishList(product: self.wishListItems[indexPath.row], user: self.user!, success: {
+            wishListTable.deleteRows(at: [indexPath], with: .right)
+            wishListTable.endUpdates()
+        }, failure: { error in
+            print(error.localizedDescription)
+            wishListTable.endUpdates()
+        })
+    }
+    
+    override func wishListChanged() {
+        super.wishListChanged()
+        self.wishListTable.reloadData()
     }
 
 }
